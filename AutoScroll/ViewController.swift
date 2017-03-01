cc223902fe5ba4ff3edcb9ef47f068e5855519e1
okumura <okumura@ap-com.co.jp>
2017-03-01 18:14:23 +0900

    Initial Commit

---
+++ AutoScroll/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoScroll
+//
+//  Created by okumura on 2017/03/01.
+//  Copyright © 2017 akariokumura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
